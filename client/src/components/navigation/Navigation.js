9d5fb5af9680ef91915ce58c2042f58dc270c7d3
Matthew Bower <mttbwr91@gmail.com>
2020-07-06 20:43:15 -0700

    more touches

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -5,7 +5,9 @@ const Navigation = () => {
   return (
     <div className="navigation">
       <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-      <label htmlFor="navi-toggle" className="navigation__button">Menu</label>
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
       <div className="navigation__background">&nbsp;</div>
       <nav className="navigation__nav">
         <ul className="navigation__list">
